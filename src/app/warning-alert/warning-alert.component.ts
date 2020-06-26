eeb1cd834ead17464dfbba0da1f0652eb52316b2
Monika <mswidzinska87@gmail.com>
2020-06-26 11:34:27 +0200

    the second approach in warning-alert

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'warning',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  // templateUrl: './warning-alert.component.html',
+  template:`<h3>Warning Alert!</h3>`,
+  styles: [
+    `
+    h3 {
+    background-color: red;
+    color: white;
+    border: 1px solid black;
+    padding: 10px 0;
+    }
+    `
+]
 })
 export class WarningAlertComponent implements OnInit {
 
